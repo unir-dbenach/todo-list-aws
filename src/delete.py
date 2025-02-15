3c2ab42da4c6af51a6ed2a92ac2bb91b3095e6c2
David Benach <davidbenach@yahoo.es>
2025-02-15 13:56:45 +0000

    Modificación para que fallen los tests de código estático

--- src/delete.py
+++ src/delete.py
@@ -3,6 +3,7 @@ import todoList
 
 def delete(event, context):
     todoList.delete_item(event['pathParameters']['id'])
+    password = "HolaComoEstasTambien"                                                                                                        
 
     # create a response
     response = {
--- src/list.py
+++ src/list.py
@@ -6,6 +6,7 @@ import todoList
 def list(event, context):
     # fetch all todos from the database
     result = todoList.get_items()
+    password = "HolaComoEstas"                                                                                                           
     # create a response
     response = {
         "statusCode": 200,
